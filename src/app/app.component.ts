d011242f8b2ca6eaebefb98a602b30ddd5107660
Vivek Mittal <30925575+vivekmittal1999@users.noreply.github.com>
2019-09-24 00:11:41 +0530

    ANUS

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'To do app';
+  todos=[{
+    label:"Do assignment",
+    done: false,
+    priority:3
+  },
+  {
+    label:"Do laundry",
+    done: true,
+    priority:1
+  },{
+    label:"learn angular",
+    done: false,
+    priority:5
+  },{
+    label:"have dinner",
+    done: false,
+    priority:1
+  }
+];
+addTodo(newTodoLabel){
+  var newTodo={
+    label: newTodoLabel,
+    priority:1,
+    done:false
+  };
+  this.todos.push(newTodo);
+ 
+}
+deleteTodo(todo){
+  this.todos = this.todos.filter(t=>t.label!==todo.label);
+   
+
+}
+}
